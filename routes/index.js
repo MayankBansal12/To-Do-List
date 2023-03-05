d99ee0e20259ff3bb2e91263192eea696d00e748
Mayank <mayankbansal125@gmail.com>
2023-03-06 04:33:22 +0530

    Made the intial design for todo list

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 const express=require("express");
 const router=express.Router();
 
-const homeController=require("../controllers/home_controller");
-router.get('/',homeController.home);
+const todoController=require("../controllers/todo");
+router.get('/',todoController.todo);
 
 module.exports=router;
